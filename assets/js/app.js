a3a07a03979f236dedb60d6d7fa035a2d9010960
Webovski <sokri4.developer@gmail.com>
2022-08-17 13:33:48 +0200

    adde buttons and basic handling

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,31 +1,37 @@
 let theme = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+
 let lat = 46.68664732483417;
 let lon = 32.54464706119928;
 
-
 let customMap = null;
-let markerClusters;
 let popup = L.popup();
+let markersArray = []
 
 function initMap() {
-
     customMap = L.map('map').setView([lat, lon], 13);
-    markerClusters = L.markerClusterGroup;
     L.tileLayer(theme, {
         minZoom: 1, maxZoom: 20
     }).addTo(customMap);
-    customMap.on('click', onMapClick);
+    customMap.on('click', setPoint);
+
 }
 
-function onMapClick(e) {
-    let latitude = e.latlng.lat
-    let longitude = e.latlng.lng
-    popup
-        .setLatLng(e.latlng)
-        .setContent(`${latitude} </br>${longitude}`)
-        .openOn(customMap);
-
+function setPoint(e) {
+    popup.setLatLng(e.latlng)
     let marker = L.marker(e.latlng).addTo(customMap)
+    markersArray.push(marker)
+}
+
+function clearPoints() {
+    for (let markerItem of markersArray) {
+        customMap.removeLayer(markerItem)
+    }
+    markersArray = []
+}
+
+function startParsing() {
+    alert(`Всего точек: ${markersArray.length}`)
+    clearPoints()
 }
 
 $(document).ready(function () {
