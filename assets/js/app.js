5b1bcb3ce92826c7740c5ccd5451ef1df668babe
Webovski <sokri4.developer@gmail.com>
2022-08-17 10:18:30 +0200

    chornobaivka

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,31 +1,33 @@
-    let theme = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-    let lat = 45.757583;
-    let lon = 24.561138;
-    let macarte = null;
-    let markerClusters;
-    let popup = L.popup();
+let theme = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+let lat = 46.68664732483417;
+let lon = 32.54464706119928;
 
-    function initMap() {
 
-        macarte = L.map('map').setView([lat, lon], 5);
-        markerClusters = L.markerClusterGroup;
-        L.tileLayer(theme, {
-            minZoom: 1,
-            maxZoom: 20
-        }).addTo(macarte);
-        macarte.on('click', onMapClick);
-    }
+let customMap = null;
+let markerClusters;
+let popup = L.popup();
 
-    function onMapClick(e) {
-        let latitude = e.latlng.lat
-        let longitude = e.latlng.lng
-        popup
-            .setLatLng(e.latlng)
-            .setContent(`${latitude} </br>${longitude}`)
-            .openOn(macarte);
-        let marker = L.marker(e.latlng).addTo(macarte)
-    }
+function initMap() {
 
-    $(document).ready(function () {
-        initMap();
-    });
+    customMap = L.map('map').setView([lat, lon], 13);
+    markerClusters = L.markerClusterGroup;
+    L.tileLayer(theme, {
+        minZoom: 1, maxZoom: 20
+    }).addTo(customMap);
+    customMap.on('click', onMapClick);
+}
+
+function onMapClick(e) {
+    let latitude = e.latlng.lat
+    let longitude = e.latlng.lng
+    popup
+        .setLatLng(e.latlng)
+        .setContent(`${latitude} </br>${longitude}`)
+        .openOn(customMap);
+
+    let marker = L.marker(e.latlng).addTo(customMap)
+}
+
+$(document).ready(function () {
+    initMap();
+});
